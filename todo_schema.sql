10eb7063d7027a4b6c5a07dd5fa14f8f2742a8d4
jananiiramesh <reachjanu2004@gmail.com>
2025-01-18 15:02:54 +0530

    changes in db

--- todo_schema.sql
+++ todo_schema.sql
@@ -2,8 +2,25 @@ DROP DATABASE IF EXISTS todo_list;
 CREATE DATABASE todo_list;
 USE todo_list;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS admin;
 CREATE TABLE users (
 	user_id SERIAL PRIMARY KEY,
     username VARCHAR(20) UNIQUE NOT NULL,
-    password VARCHAR(20) NOT NULL
+    password VARCHAR(255) NOT NULL
+);
+CREATE TABLE admin (
+    admin_id SERIAL PRIMARY KEY,
+    username VARCHAR(20) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+CREATE TABLE tasks (
+    task_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    name VARCHAR(20) NOT NULL,
+    description TEXT,
+    status ENUM('Completed','Ongoing','Yet to start') NOT NULL DEFAULT 'Yet to start',
+    createdOn DATE DEFAULT CURRENT_TIMESTAMP(),
+    due_date DATE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
